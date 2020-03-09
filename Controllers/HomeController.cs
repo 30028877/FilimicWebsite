4735547ca58f58d8038059eda22c2ad59d918a35
ZunZiAc <rajkirantmkr@gmail.com>
2020-03-09 19:24:30 +1300

    This commit have  all source code.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace filimic.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Map()
+        {
+            return View();
+        }
+
+        public ActionResult homev2()
+        {
+            return View();
+        }
+
+        public ActionResult PageNotFount()
+        {
+            return View();
+        }
+        public ActionResult BlogDetail()
+        {
+            return View();
+        }
+        public ActionResult BlogGrid()
+        {
+            return View();
+        }
+        public ActionResult BlogList()
+        {
+            return View();
+        }
+        public ActionResult CelebrityGrid()
+        {
+            return View();
+        }
+
+        public ActionResult CelebrityGrid2()
+        {
+            return View();
+        }
+        public ActionResult CelebrityList()
+        {
+            return View();
+        }
+        public ActionResult CelebritySingle()
+        {
+            return View();
+        }
+        public ActionResult CommingSoon()
+        {
+            return View();
+        }
+        public ActionResult Landing()
+        {
+            return View();
+        }
+
+        public ActionResult MovieGrid()
+        {
+            return View();
+        }
+
+        public ActionResult MovieGridFullWidth()
+        {
+            return View();
+        }
+
+        public ActionResult MovieList()
+        {
+            return View();
+        }
+        public ActionResult MovieSingle()
+        {
+            return View();
+        }
+        public ActionResult SeriesSingle()
+        {
+            return View();
+        }
+
+        public ActionResult UserfavoriteGrid()
+        {
+            return View();
+        }
+        public ActionResult UserProfile()
+        {
+            return View();
+        }
+        public ActionResult UserRate()
+        {
+            return View();
+        }
+    }
+}
